17701c6db0c07ed46f810c25c5c9aa5a8c737950
Rezan <rezan@kizilguen.de>
2016-06-15 15:14:48 +0200

    Added Apex Utility AI plugin

---
+++ Assets/QueenOfTheJungle/Characters/AI/AIContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Apex.AI;
+using Apex.AI.Components;
+
+namespace QJAI
+{
+    public class AIContext : IAIContext
+    {
+        /// <summary>
+        /// Constructor that initializes the context provider
+        /// </summary>
+        /// <param name="contextProvider">Context provider that owns this context object</param>
+        public AIContext(IContextProvider contextProvider)
+        {
+            ContextProvider = contextProvider;
+        }
+
+        /// <summary>
+        /// Gets the context provider owning this context object
+        /// </summary>
+        public IContextProvider ContextProvider
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ Assets/QueenOfTheJungle/Characters/AI/UtilityAI.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using Apex.AI;
+using Apex.AI.Components;
+
+namespace QJAI
+{
+    [ExecuteInEditMode]
+    public class UtilityAI : MonoBehaviour, IContextProvider
+    {
+        /// <summary>
+        /// AI context object for the Utility AI system
+        /// </summary>
+        private IAIContext aiContext = null;
+
+        /// <summary>
+        /// Utility AI component that gets created by this component
+        /// </summary>
+        private UtilityAIComponent utilityAIComp = null;
+
+        void Awake()
+        {
+            // Create new context object if it isn't valid
+            if(aiContext == null)
+                aiContext = new AIContext(this);
+
+            // Get or create Utility AI component if it isn't valid
+            if (utilityAIComp == null)
+            {
+                utilityAIComp = GetComponent<UtilityAIComponent>();
+                if(utilityAIComp == null)
+                    utilityAIComp = gameObject.AddComponent<UtilityAIComponent>();
+                if (utilityAIComp == null)
+                    Debug.LogError("Utility AI component creation failed!");
+            }
+        }
+
+        /// <summary>
+        /// Get context method for Utility AI system
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IAIContext GetContext(System.Guid id)
+        {
+            return aiContext;
+        }
+    }
+}
